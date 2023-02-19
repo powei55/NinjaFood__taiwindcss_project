0174d0eb337f8ff05812a4142ca75e5d8d7cfbca
powel ikechukwu <ikechukwupowei21@gmail.com>
2023-02-19 03:36:31 +0100

    committing first taiwindcss project

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#ff6363',
+        secondary: {
+          100: '#E2E2D5',
+          200: '#888883'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
